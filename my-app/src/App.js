9378ea29f25d62167347317e2b8b6d1686a8b8c7
Oraz Makhmetov <omakh003@fiu.edu>
2023-06-23 00:07:35 -0400

    Reversed Index.js to original, added hashrouting to app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Header from './Header';
 import './tailwind.css';
 import SearchMovies from './Search';
+import { HashRouter } from "react-router-dom";
 
 function App() {
 
   return (
+    <HashRouter base="/">
       <div className="relative width-full">
         <div className="mx-auto overflow-hidden">
-          <Header/>
-          <SearchMovies/>
+          <Header />
+          <SearchMovies />
         </div>
       </div>
+    </HashRouter>
   );
 }
 
